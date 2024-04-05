02b0264e358879108cd764f26c8595ae76b4b6ac
Afonso Morvillo <afonsomorvillo1@gmail.com>
2024-04-04 22:40:30 -0300

    Tela splash

--- projetofinaldmo/app/src/main/java/br/edu/ifsp/arq/ads/dmo/SplashActivity.kt
+++ projetofinaldmo/app/src/main/java/br/edu/ifsp/arq/ads/dmo/SplashActivity.kt
@@ -1,23 +1,20 @@
 package br.edu.ifsp.arq.ads.dmo
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-
-        val intent = Intent(this, MainActivity::class.java)
-        val handler = Handler()
-        handler.postDelayed(Runnable {
-            startActivity(intent)
-            finish()
-        }, 3000)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
