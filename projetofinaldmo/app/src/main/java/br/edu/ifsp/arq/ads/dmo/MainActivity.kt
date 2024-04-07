ebbff2ed3c0265ba34a84f2457cfd052c358654a
Afonso Morvillo <afonsomorvillo1@gmail.com>
2024-04-07 17:35:00 -0300

    Revert "Tela splash"
    This reverts commit 02b0264e358879108cd764f26c8595ae76b4b6ac.

--- projetofinaldmo/app/src/main/java/br/edu/ifsp/arq/ads/dmo/MainActivity.kt
+++ projetofinaldmo/app/src/main/java/br/edu/ifsp/arq/ads/dmo/MainActivity.kt
@@ -1,8 +1,6 @@
 package br.edu.ifsp.arq.ads.dmo
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,13 +9,12 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
-        val intent = Intent(this, MainActivity::class.java)
-        val handler = Handler()
-        handler.postDelayed(Runnable {
-            startActivity(intent)
-            finish()
-        }, 3000)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
