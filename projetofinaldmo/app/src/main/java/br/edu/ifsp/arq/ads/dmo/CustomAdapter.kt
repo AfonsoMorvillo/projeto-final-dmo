1feaf32a662a46fd005adf0a25eb94c46db8f904
Afonso <AFonso Morvillo>
2024-07-15 09:21:36 -0300

    Mudança icone e adição onClick do card

--- projetofinaldmo/app/src/main/java/br/edu/ifsp/arq/ads/dmo/CustomAdapter.kt
+++ projetofinaldmo/app/src/main/java/br/edu/ifsp/arq/ads/dmo/CustomAdapter.kt
@@ -3,16 +3,21 @@ package br.edu.ifsp.arq.ads.dmo
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class CustomAdapter: RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+class CustomAdapter(private val listener: OnItemClickListener): RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
     val titles = arrayOf("Grupo A", "Grupo B")
     val details = arrayOf("Descricao A", "Descricao B")
 
     val images = intArrayOf(R.drawable.menu_vazio, R.drawable.logo_verde)
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
         val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.card_layout,viewGroup,false)
@@ -23,6 +28,10 @@ class CustomAdapter: RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
         viewHolder.itemTitle.text = titles[i]
         viewHolder.itemDetail.text = details[i]
         viewHolder.itemImage.setImageResource( images[i])
+
+        viewHolder.itemView.setOnClickListener {
+            listener.onItemClick(i) // Notifica o listener quando o card é clicado
+        }
     }
 
     override fun getItemCount(): Int {
@@ -38,6 +47,9 @@ class CustomAdapter: RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
             itemImage = itemView.findViewById(R.id.item_image)
             itemTitle = itemView.findViewById(R.id.item_title)
             itemDetail = itemView.findViewById(R.id.item_detail)
+            itemView.setOnClickListener {
+                listener.onItemClick(adapterPosition)
+            }
         }
     }
 }
--- projetofinaldmo/app/src/main/java/br/edu/ifsp/arq/ads/dmo/ListaGruposActivity.kt
+++ projetofinaldmo/app/src/main/java/br/edu/ifsp/arq/ads/dmo/ListaGruposActivity.kt
@@ -1,6 +1,7 @@
 package br.edu.ifsp.arq.ads.dmo
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,17 +9,23 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class ListaGruposActivity : AppCompatActivity() {
+class ListaGruposActivity : AppCompatActivity(), CustomAdapter.OnItemClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.supportActionBar?.hide()
         setContentView(R.layout.activity_lista_grupos)
 
         val recycylerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val adapter = CustomAdapter()
+        val adapter = CustomAdapter(this)
 
         recycylerView.layoutManager = LinearLayoutManager(this)
         recycylerView.adapter = adapter
+
+    }
+
+    override fun onItemClick(position: Int) {
+
+        Toast.makeText(this, "Clicou no item $position", Toast.LENGTH_SHORT).show()
 
     }
 }
